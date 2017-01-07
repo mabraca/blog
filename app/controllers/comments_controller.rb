8221f0060377b060a5382d213a00783d84d7af52
Maria Bracamonte <mabraca18@gmail.com>
2017-01-07 00:23:13 -0400

    Enlaces

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -24,12 +24,15 @@ class CommentsController < ApplicationController
   # POST /comments
   # POST /comments.json
   def create
-    @comment = Comment.new(comment_params)
+    @post = Post.find(params[:post_id]) #no estaba
+    @comment = @post.comments.create(comment_params) #no estaba
+    #@comment = Comment.new(comment_params) #estaba este
 
     respond_to do |format|
       if @comment.save
         format.html { redirect_to @comment, notice: 'Comment was successfully created.' }
         format.json { render :show, status: :created, location: @comment }
+        format.js #create js.erb
       else
         format.html { render :new }
         format.json { render json: @comment.errors, status: :unprocessable_entity }
